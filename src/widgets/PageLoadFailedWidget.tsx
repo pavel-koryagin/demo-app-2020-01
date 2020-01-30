415aedcf8c26a79608b55cc3400a2ae5cdcf82fd
Pavel Koryagin <pavel@koryagin.com>
2020-01-31 03:42:55 +0700

    Import Load Failed Widget

--- src/widgets/PageLoadFailedWidget.tsx
+++ src/widgets/PageLoadFailedWidget.tsx
@@ -5,9 +5,34 @@ interface Props {
   error: string | Error | ErrorCapsule,
 }
 
+function decodeException(error: Error) {
+  return error.message;
+}
+
+function decodeError(error: string | Error | ErrorCapsule) {
+  if (error instanceof ErrorCapsule) {
+    return { message: decodeException(error.error), onRetry: error.retry };
+  }
+  if (error instanceof Error) {
+    return { message: decodeException(error), onRetry: null };
+  }
+  return { message: error, onRetry: null };
+}
+
 const PageLoadFailedWidget: React.FC<Props> = ({ error }: Props) => {
+  const { message, onRetry } = decodeError(error);
+
+  // TODO: Replace with Material UI
   return (
-    <div>TODO Show load error</div>
+    <div className="alert alert-danger">
+      <p>
+        <strong>Load failed.</strong>
+        {onRetry && (
+          <button type="button" className="btn btn-link" onClick={onRetry}>Click to retry</button>
+        )}
+      </p>
+      <p>{message}</p>
+    </div>
   );
 };
 
